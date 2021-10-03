47f9416971b195a5c59504f4c37a6d52f0c29598
Matthew <matthewstephenson39@gmail.com>
2021-10-02 19:17:42 -0700

    added code to follow script path to find python script

--- stacker.sh
+++ stacker.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
+# Author: Matthew Stephenson
+# 2021-10-02 Hackathon Project
+# Sends unused files and folders into a stacks folder in the directory where called
+
+#code for finding script path from https://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+SCRIPTPATH=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPTPATH")
+PYSCRIPT="/stacker.py"
+PYSCRIPTPATH="$SCRIPTDIR$PYSCRIPT"
 
 echo #newline for aesthetics
-ls -cltr #print files with dates to help user
+ls -cltr #print files with dates to help user decide the input date
 echo
 
-read -p 'Files sent to stack when last modified before the date (yyyy-mm-dd): ' date
-#check for stacks folder and create one if not there
+read -p 'Files sent to stack when last modified before the date (yyyy-mm-dd): ' DATE
 
 mkdir -p stacks #make stacks folder if not there, with -p option so no error if existing
 
-ls -cltr | python stacker.py $date | while read file; do #each line is a file that will be moved
+ls -cltr | python $PYSCRIPTPATH $DATE | while read FILE; do #each line is a file that will be moved
     #could add interactive option and check each file here
-    mv $file stacks
+    echo $FILE " moved to stacks"
+    mv $FILE stacks
 done
 
